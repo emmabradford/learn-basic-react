e6ca454cb3aebb43fdf90a49b35046c8c1b177be
EmmaAbe <emma.abe.bradford@gmail.com>
2021-07-08 16:25:59 -0500

    making friedno a function

--- src/components/Friendo.js
+++ src/components/Friendo.js
@@ -1,24 +1,21 @@
 import React from 'react';
 
-class Friendo extends React.Component
+const Friendo = ({friendo}) =>
 {
-    render(){
-        const {friendo} = this.props;
-        const friendoList = friendo.map(fernd => {
-            return(
-                <div className="friendo" key={friendo.id}>
-                    <div>Name: {fernd.name}</div>
-                    <div>Age: {fernd.age}</div>
-                    <div>Weapon: {fernd.weapon}</div>
-                </div>
-            )
-        })
+    const friendoList = friendo.map(fernd => {
         return(
-            <div className="firendo-list">
-                {friendoList}
+            <div className="friendo" key={friendo.id}>
+                <div>Name: {fernd.name}</div>
+                <div>Age: {fernd.age}</div>
+                <div>Weapon: {fernd.weapon}</div>
             </div>
         )
-    }
+    })
+    return(
+        <div className="firendo-list">
+            {friendoList}
+        </div>
+    )
 }
 
 export default Friendo;
